5cd32d53d9ac4a2836d48d2dfe9c5256b74c071a
Yakub ONAL <yakubonal6693@gmail.com>
2024-10-03 17:00:50 +0200

    Installation package domPDF

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/posts/show.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Post PDF</title>
+</head>
+<body>
+    <h1>{{ $post->title }}</h1>
+    <p>{{ $post->content }}</p>
+</body>
+</html>
