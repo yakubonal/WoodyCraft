f6d5f3af99b46078fc9df777138ca9de21b9e82d
yakubonal <yakubonal6601@gmail.com>
2024-10-14 01:21:26 +0200

    panier correction fonction

--- app/Http/Controllers/PanierController.php
+++ app/Http/Controllers/PanierController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 
 class PanierController extends Controller
 {
-    // Affiche le contenu du panier
-    public function index(Request $request)
+    // Obtient le panier s'il existe, sinon le créé
+    public function get_panier(Request $request)
     {
         // Obtention de l'id de l'utilisateur connecté
         $user = auth()->user();
@@ -32,6 +32,14 @@ class PanierController extends Controller
                 ]);
             }
         }
+
+        return $panier;
+    }
+
+    // Affiche le contenu du panier
+    public function index(Request $request)
+    {
+        $panier = $this->get_panier($request);
 
         // Obtention de la liste des produits du panier
         $produits = Produit::with('articles')
@@ -43,41 +51,10 @@ class PanierController extends Controller
         return view('produits/panier', ['produits' => $produits]);
     }
 
-    // Si l'user est connecté
-    //     Si il a un panier
-    //         Récupération de l'id de son panier
-    //     Si aucun panier n'est associé
-    //         Création du panier
-    // Si l'user n'est pas connecté
-    //     Si il a un panier
-    //         Récupération du panier associé
-    //     Si aucun panier n'est associé
-    //         Création d'un panier avec un ID unique à cet utilisateur (à sa session), comme par exemple : session_id
-
     // Fonction qui permet d'ajouter un produit au panier
     public function ajout(Request $request)
     {
-        // Obtention de l'id de l'utilisateur connecté
-        $user = auth()->user();
-
-        // Si l'user est connecté
-        if (! is_null($user))
-        {
-            $panier = Panier::findOrFail($user->panier_id);
-        }
-        // Si l'user n'est pas connecté (invité)
-        else
-        {
-            $session_id = $request->session()->getId();
-            $panier = Panier::where('session_id', $session_id)->first();
-
-            // Si le panier n'existe pas, on le créé
-            if ($panier === null) {
-                $panier = Panier::create([
-                    'session_id' => $session_id,
-                ]);
-            }
-        }
+        $panier = $this->get_panier($request);
 
         // TODO Si le produit est déjà dans le panier, modifier sa quantité
 
@@ -88,14 +65,7 @@ class PanierController extends Controller
             'quantity' => $request->quantity,
         ]);
 
-        // Obtention de la liste des produits du panier
-        $produits = Produit::with('articles')
-        ->join('article_panier', 'article_panier.produit_id', '=', 'produit.id')
-        ->join('panier', 'article_panier.panier_id', '=', 'panier.id')
-        ->where('panier.id', $panier->id)
-        ->get();
-
         // Affichage du panier
-        return view('produits/panier', ['produits' => $produits]);
+        return redirect('/panier');
     }
 }
