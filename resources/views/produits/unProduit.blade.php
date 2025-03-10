965bfc5076091d929bdb79ab1a9eeceae92f4ae0
Yakub ONAL <yakubonal6601@gmail.com>
2025-03-10 10:15:08 +0100

    Visuel d'un produit

--- resources/views/produits/unProduit.blade.php
+++ resources/views/produits/unProduit.blade.php
@@ -1,27 +1,30 @@
 <x-app-layout>
     <div class="container my-5">
-        <h1 class="text-center">{{ $produit->nom }}</h1>
-        <div class="row">
-            <div class="col-md-6">
-                <!-- Vérification si l'image existe avant de l'afficher -->
-                @if ($produit->image_url)
-                    <img src="{{ $produit->image_url }}" class="img-fluid" alt="{{ $produit->nom }}">
-                @else
-                    <img src="{{ asset('images/default-image-url.png') }}" class="img-fluid" alt="Image par défaut">
-                @endif
-            </div>
-            <div class="col-md-6">
-                <h5>Description :</h5>
-                <p>{{ $produit->description }}</p>
-                <p><strong>Prix : {{ number_format($produit->prix, 2) }} €</strong></p>
-                <form method="POST" action="{{ route('panier.ajout') }}">
-                    @csrf
-                    <input type="hidden" name="produit_id" value="{{ $produit->id }}">
-                    <input type="number" name="quantity" placeholder="Quantité ?" class="form-control mr-2" min="0" max="10" >
-                    <button type="submit" class="btn btn-primary">Ajouter au panier</a>
-                </form>
+        <div class="container my-5 position-relative">
+            <!-- Bouton "Retour" légèrement plus haut que le titre -->
+            <a href="{{ route('lesproduits.index') }}" class="btn btn-secondary position-absolute start-0" style="top: -10px;">Retour</a>
+            <h1 class="text-center my-5 py-4">{{ $produit->nom }}</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    @if ($produit->image_url)
+                        <img src="{{ $produit->image_url }}" class="img-fluid" alt="{{ $produit->nom }}">
+                    @else
+                        <img src="{{ asset('images/default-image-url.png') }}" class="img-fluid" alt="Image par défaut">
+                    @endif
+                </div>
+                <div class="col-md-6">
+                    <h5>Description :</h5>
+                    <p>{{ $produit->description }}</p>
+                    <p><strong>Prix : {{ number_format($produit->prix, 2) }} €</strong></p>
+                    
+                    <form method="POST" action="{{ route('panier.ajout') }}">
+                        @csrf
+                        <input type="hidden" name="produit_id" value="{{ $produit->id }}">
+                        <input type="number" name="quantity" placeholder="Quantité ?" class="form-control mb-2" min="0" max="10">
+                        <button type="submit" class="btn btn-primary">Ajouter au panier</button>
+                    </form>
+                </div>
             </div>
         </div>
-        <a href="{{ route('lesproduits.index') }}" class="btn btn-secondary mt-4">Retour</a>
     </div>
 </x-app-layout>
