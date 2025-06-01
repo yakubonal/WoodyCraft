ea608a6f8450ca3fe92d69b3109fcb14eb2cb70e
yakubonal <yakubonal6601@gmail.com>
2025-06-01 21:49:06 +0200

    fix html wishlist

--- resources/views/produits/wishlist.blade.php
+++ resources/views/produits/wishlist.blade.php
@@ -17,40 +17,40 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fermer"></button>
             </div>
         @endif
-    </div>
-    <div class="row">
-        <div class="col s12 cards-container">
-            @if(isset($message))
-            <div class="alert alert-warning text-center">
-                {{ $message }}
-            </div>
-            @endif
+        <div class="row">
+            <div class="col s12 cards-container">
+                @if(isset($message))
+                <div class="alert alert-warning text-center">
+                    {{ $message }}
+                </div>
+                @endif
 
-            @foreach ($produits as $produit)
-                <div class="card mb-4">
-                    <div class="row g-0 d-flex align-items-center">
-                        <!-- Affichage de l'image -->
-                        <div class="col-md-4">
-                            @if ($produit->image_url)
-                                <img src="{{ $produit->image_url }}" alt="{{ $produit->nom }}" class="img-fluid rounded" style="width: 100%; height: 100%; object-fit: cover;">
-                            @else
-                                <img src="{{ asset('images/default-image-url.png') }}" alt="Image par défaut" class="img-fluid rounded" style="width: 100%; height: 100%; object-fit: cover;">
-                            @endif
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $produit->nom }}</h5>
-                                <p class="card-text">{{ $produit->description }}</p>
-                                <p class="card-text">Prix : {{ number_format($produit->prix, 2) }} €</p>
-                                <div class="d-flex align-items-center mb-3">
-                                    <!-- Affichage de la quantité -->
-                                    <span>{{ $produit->quantity }}</span>
+                @foreach ($produits as $produit)
+                    <div class="card mb-4">
+                        <div class="row g-0 d-flex align-items-center">
+                            <!-- Affichage de l'image -->
+                            <div class="col-md-4">
+                                @if ($produit->image_url)
+                                    <img src="{{ $produit->image_url }}" alt="{{ $produit->nom }}" class="img-fluid rounded" style="width: 100%; height: 100%; object-fit: cover;">
+                                @else
+                                    <img src="{{ asset('images/default-image-url.png') }}" alt="Image par défaut" class="img-fluid rounded" style="width: 100%; height: 100%; object-fit: cover;">
+                                @endif
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $produit->nom }}</h5>
+                                    <p class="card-text">{{ $produit->description }}</p>
+                                    <p class="card-text">Prix : {{ number_format($produit->prix, 2) }} €</p>
+                                    <div class="d-flex align-items-center mb-3">
+                                        <!-- Affichage de la quantité -->
+                                        <span>{{ $produit->quantity }}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 </x-app-layout>
